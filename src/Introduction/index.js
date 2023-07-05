c197a696fe63be92dc90f261febcbe0d4ad5992d
orishaba-mercy <orishabamercybanga@gmail.com>
2023-07-06 00:01:59 +0300

    changes

---
+++ src/Introduction/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './style.css'
+//pascalCsing capitalise
+const Introduction =()=>{
+    //jsx:javascript xml
+     //changes to js
+    return(
+        <div className="order">
+             <div className='head'>
+                <li>Home</li>
+                <li>Services</li>
+                <li>Abouts</li>
+                <li>Contact</li>
+                
+            </div>
+            <h1> What i learnt in react</h1>
+            <p>It is a js library developed by Facebook</p>
+            <p>It has reuseable components</p>
+        </div>
+    )
+}
+export default Introduction;
---
+++ src/Products/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.css'
+
+
+
+
+const Products =()=>{
+    return(
+        <div className='header'>
+            <h2> I love react</h2>
+            <p>It makes things easy and fast</p>
+            <p>lets all use react</p>
+            {/* <img src="orange.jpeg" alt=''/> */}
+        </div>
+
+    )
+    
+}
+export default Products;
